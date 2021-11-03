18ca963367863603f3d19aaabb96246d6d851329
levxyca <leticiacaroline-123@hotmail.com>
2021-11-03 11:22:05 -0300

    :sparkles: consumindo api foodish

--- js/app.js
+++ js/app.js
@@ -1,52 +1,16 @@
-const btnModalLogin = document.getElementById("login__submit");
-const btnModalRegister = document.getElementById("register__submit");
+const search = document.getElementById("app_btn");
 
-btnModalLogin.addEventListener("click", () => {
-  const user = document.getElementById("user");
-  const pass = document.getElementById("pass");
-
-  let login = {
-    email: user.value,
-    password: pass.value,
-  };
-
+search.addEventListener("click", () => {
   var ajax = new XMLHttpRequest();
 
-  ajax.open("POST", "https://reqres.in/api/login", true);
-  ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  ajax.open("GET", "https://foodish-api.herokuapp.com/api", true);
 
-  ajax.send(`email=${login.email}&password=${login.password}`);
+  ajax.send();
 
   ajax.onreadystatechange = function () {
     if (ajax.status == 200) {
       var data = ajax.responseText;
-      console.log(data);
-    } else {
-      console.log("ERRO");
-    }
-  };
-});
-
-btnModalRegister.addEventListener("click", () => {
-  const user = document.getElementById("userRegister");
-  const pass = document.getElementById("passRegister");
-
-  let register = {
-    email: user.value,
-    password: pass.value,
-  };
-
-  var ajax = new XMLHttpRequest();
-
-  ajax.open("POST", "https://reqres.in/api/register", true);
-  ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-  ajax.send(`email=${register.email}&password=${register.password}`);
-
-  ajax.onreadystatechange = function () {
-    if (ajax.status == 200) {
-      var data = ajax.responseText;
-      console.log(data);
+      document.getElementById("app__image").src = data.slice(10, -2);
     } else {
       console.log("ERRO");
     }
